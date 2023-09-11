4cf634025e63dfc978341a3f8a5f6951ca9ed1d8
John Doe <betiaman@example.com>
2023-09-11 03:39:54 -0500

    finishing home page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,86 @@
-// JavaScript (script.js)
-const textToType = "Hello World! Welcome to my portfolio." 
+//typing effect 
+var p = document.getElementById('text');
+var texts = ['Front End Developer', 'React Developer', 'Web Developer', 'Junior Developer'];
+var currentIndex = 0;
+var currentText = texts[currentIndex];
+var n = 0;
+var typing = true;
 
-+ "I'm Betiel Amanuel, a recent graduate of a React Program at PerScholas powered by TEKsystems. "
-+ "I specialize in creating responsive web applications using React, HTML, CSS, and JavaScript. "
-+ "I'm passionate about building user-friendly interfaces and have a strong foundation in front-end development. " 
-+ "Please take a moment to explore some of my projects and get to know my skills and experience.";
-const typingSpeed = 50; // in milliseconds
-const typingContainer = document.getElementById("typing-text");
-
-let charIndex = 0;
-
-function typeText() {
-    if (charIndex < textToType.length) {
-        typingContainer.innerHTML += textToType.charAt(charIndex);
-        charIndex++;
-        setTimeout(typeText, typingSpeed);
+function toggleCursor() {
+    if (n % 2 === 0) {
+        p.innerHTML = " " + currentText + "|";
+    } else {
+        p.innerHTML = " " + currentText ;
     }
 }
 
-// Start the typing effect when the page loads
-window.addEventListener("load", typeText);
+function typeText() {
+    if (typing) {
+        if (n <= currentText.length) {
+            p.innerHTML = "" + currentText.slice(0, n) + "";
+            n++;
+        } else {
+            clearInterval(typeTimer);
+            typing = false;
+            setTimeout(function () {
+                currentIndex = (currentIndex + 1) % texts.length;
+                currentText = texts[currentIndex];
+                n = 0;
+                setTimeout(function () {
+                    typing = true;
+                    typeTimer = setInterval(typeText, 250); // Delay before starting next text
+                }, 500); // Delay before switching to the next text
+            }, 500); // Delay before starting next text
+        }
+    } else {
+        toggleCursor();
+    }
+}
+
+var typeTimer = setInterval(typeText, 100);
+
+// button sound effect
+document.addEventListener("DOMContentLoaded", function () {
+    // Get references to all buttons and the audio element
+    const buttons = document.querySelectorAll(".clickable-butt");
+    const audio = document.getElementById("click-sound");
+
+    // Add a click event listener to each button
+    buttons.forEach((button) => {
+        button.addEventListener("click", () => {
+            // Play the audio when any button is clicked
+            audio.play();
+        });
+    });
+});
+
+// profile-botton image transtion
+// $(document).ready(function() {
+//     var isImageVisible = true; // Keep track of whether the image is currently visible
+
+//     // Function to toggle between image and button
+//     function toggleImageAndButton() {
+//     if (isImageVisible) {
+//         // If the image is visible, fade it out and show the button
+//         $('#profile-image').fadeOut(400, function() {
+//         $('#learn-more-button').fadeIn(400);
+//         });
+//     } else {
+//         // If the button is visible, fade it out and show the image
+//         $('#learn-more-button').fadeOut(400, function() {
+//         $('#profile-image').fadeIn(400);
+//         });
+//     }
+//     isImageVisible = !isImageVisible; // Toggle the visibility flag
+//     }
+
+//     // Set an interval to trigger the toggle every 5 seconds (5000 milliseconds)
+//     setInterval(toggleImageAndButton, 5000); // 5000 milliseconds (5 seconds)
+
+//     // Add a click event handler to the "Learn More" button
+//     $('#learn-more-button').click(function() {
+//     // Redirect to a page with more information about you
+//     window.location.href = 'your-profile-page.html';
+//     });
+// });
+
