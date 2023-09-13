a3d0f5a4d57c022179d7c192045317a4659db039
John Doe <betiaman@example.com>
2023-09-13 02:21:57 -0500

    adding page transition

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,56 @@
+window.onload = () => {
+    const transition_el = document.querySelector('.transition');
+    const anchors = document.querySelectorAll('a');
+
+    setTimeout(() =>{
+        transition_el.classList.remove('is-active');
+    }, 500);
+
+    for(let i = 0; i < anchors.length;i++){
+        const anchor = anchors[i];
+
+        anchor.addEventListener('click', e =>{
+            e.preventDefault();
+            let target = e.target.href;
+            // console.log(target);
+            transition_el.classList.add('is-active');
+
+            setTimeout(() => {
+                window.location.href = target;
+            }, 500)
+        });
+    }
+}
+
+
+
+// Landing page transition
+const tl = gsap.timeline({defaults:{ease:'power1.out'}});
+
+tl.to('.text',{
+    y:'0%',
+    duration:1,
+    stagger: 0.25
+})
+
+tl.to('.landing-transition',{
+    y:"-100%",
+    duration: 1.5,
+    delay: 0.5
+})
+
+tl.to('.intro', {
+    y: "-100%",
+    duration:1
+}, "-=1")
+
+tl.fromTo('.clickable-butt', {opacity:0}, {opacity: 1, duration:1})
+tl.fromTo('.content', {opacity:0}, {opacity: 1, duration:0.5})
+// tl.fromTo('#container', {opacity:0}, {opacity: 1, duration:0.5})
+// tl.fromTo('#text', {opacity:0}, {opacity: 1, duration:0.5})
+// tl.fromTo('#text-intro', {opacity:0}, {opacity: 1, duration:0.5})
+
+
 //typing effect 
 var p = document.getElementById('text');
 var texts = ['Front End Developer', 'React Developer', 'Web Developer', 'Junior Developer'];
